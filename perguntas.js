e6041c3d7191defc4bf6ff76d78987bafc0f6cb1
Kaualindo12 <80544643+Kiny1@users.noreply.github.com>
2024-11-18 07:40:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Programação',
+    'os principais linguagens da programação?',
+    'Python.C#, C++, JavaScript, PHP, Swift, Java, Go')
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dos Estads unidos?',
+    'Washington, D.C'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um C++?',
+    'é uma linguagem de programação que desempenha um papel fundamental no desenvolvimento de software, destacando-se por sua versatilidade e eficiência'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz te amo, karina chata em Inglês?',
+    'I love you, annoying Karina'
+)
